337c53942bfbdffe9ee470725a9a4af87c0238dc
ipeglin <philip@eglin.no>
2024-05-27 23:15:18 +0200

    chore: adding custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,28 +6,41 @@ export default {
 		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
 	],
 
+	// darkMode: 'class',
+
 	theme: {
 		extend: {
 			colors: {
 				// flowbite-svelte
 				primary: {
-					700: '#0f8592'
+					700: '#0f8592',
+					DEFAULT: '#0f8592'
 				},
 				success: {
-					700: '#52d300'
+					700: '#52d300',
+					DEFAULT: '#52d300'
 				},
 				danger: {
-					700: '#fa7736'
+					700: '#fa7736',
+					DEFAULT: '#fa7736'
 				},
 				warning: {
-					700: '#ffcb63'
+					700: '#ffcb63',
+					DEFAULT: '#ffcb63'
 				},
 				subtle: {
-					700: '#e5e9e2'
+					700: '#e5e9e2',
+					DEFAULT: '#e5e9e2'
+				},
+				black: {
+					DEFAULT: '#121212'
 				}
 			}
 		}
 	},
 
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [
+		require('@tailwindcss/typography'), 
+		// require('flowbite/plugin')
+	]
 } as Config;
