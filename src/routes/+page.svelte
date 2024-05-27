51fac1d016a6834cb7c33fa142fe74e8812186f3
ipeglin <philip@eglin.no>
2024-05-27 23:15:01 +0200

    Adding a short greeting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 
-<section id="banner">
-  <h1>Welcome</h1>
+<section id="banner" class="w-full flex flex-col justify-center text-center">
+  <h1 class="text-black font-bold">Welcome Elsys students 👋</h1>
+  <span>An ensamble of tools created by Elsys, for Elsys</span>
 </section>
+
+<style lang="scss">
+  #banner {
+    height: 50vh;
+    max-height: 500px;
+  }
+</style>
